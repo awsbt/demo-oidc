f34cfa7bb5ac354a52bae250fdef687cb0116360
Bill Tsai <bill_tsai_va@hotmail.com>
2021-10-19 10:21:31 -0400

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   sso_api_username: 'clientOIDC_0',
-  sso_api_pwd: '',
+  sso_api_pwd: 'njX7v0ykjp0xALEG2LnoSo89',
 
   loginURL: 'https://acme.example.com:443/am/oauth2/authorize?' +
               'client_id=clientOIDC_0&response_type=code&scope=openid+profile+email&' +
