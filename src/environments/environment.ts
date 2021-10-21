94204b6046925852b414fafeb3db3462ade7a6a1
Bill Tsai <bill_tsai_va@hotmail.com>
2021-10-21 16:21:11 -0400

    new domain name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,27 +2,26 @@ export const environment = {
   production: false,
 
   sso_api_username: 'clientOIDC_0',
-  sso_api_pwd: 'njX7v0ykjp0xALEG2LnoSo89',
+  sso_api_pwd: '',
 
-  loginURL: 'https://acme.example.com:443/am/oauth2/authorize?' +
+  loginURL: 'https://awsfroidcsaml2.tk:443/am/oauth2/authorize?' +
               'client_id=clientOIDC_0&response_type=code&scope=openid+profile+email&' +
               'redirect_uri=http://localhost:4200/callback',
 
   redirectURL: 'http://localhost:4200/callback',
 
-  /* FR OIDC CORS policy prevent angular to call this endpoint from localhost: https://acme.example.com:443/am/oauth2/access_token
+  /* FR OIDC CORS policy prevent angular to call this endpoint from localhost: https://awsfroidcsaml2.tk:443/am/oauth2/access_token
   * created proxy.conf.json to bypass. --make it a backchannel flow.
   * ng serve --proxy-config proxy.conf.json
   */
   oidcTokenURL: 'http://localhost:4200/token',
 
-  //todo
   detailURL: 'http://localhost:4200/detail',
 
   //todo
-  logout: 'https://acme.example.com/am/oauth2/connect/endSession'
+  logout: 'https://awsfroidcsaml2.tk/am/oauth2/connect/endSession'
 
-  //logout: 'https://acme.example.com:443/am/oauth2/logout?' +
+  //logout: 'https://awsfroidcsaml2.tk:443/am/oauth2/logout?' +
   //        'client_id=clientOIDC_0&' +
   //        'logout_uri=http://localhost:4200/home'
 };
