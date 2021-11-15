1ee663bb856ce6852c6d4ae25d8b0c3f77ab28e3
Bill Tsai <bill_tsai_va@hotmail.com>
2021-11-15 09:23:28 -0500

    add claims

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
   sso_api_pwd: 'qWVP4sCFULjcdUGm9euYJAHd',
 
   loginURL: 'https://ad7bec1a5d4a448ffb3673fc9a150873-aea79b9aae3a10f6.elb.us-gov-east-1.amazonaws.com:443/am/oauth2/authorize?' +
-              'client_id=clientOIDC_0&response_type=code&scope=openid+profile+email&' +
+              'client_id=clientOIDC_0&response_type=code&scope=openid+profile+email+project+department&' +
               'redirect_uri=http://localhost:4200/callback',
 
   awsConsoleLoginURL: 'https://ad7bec1a5d4a448ffb3673fc9a150873-aea79b9aae3a10f6.elb.us-gov-east-1.amazonaws.com/am/saml2/jsp/idpSSOInit.jsp?metaAlias=/meta-idp&spEntityID=urn:amazon:webservices:govcloud',              
